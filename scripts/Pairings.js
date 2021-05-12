a795f4774370d3915dfa189f41669830fc56c764
JoyceRuobingZhang <joyceruobingzhang@gmail.com>
2021-05-11 22:27:43 -0400

    modified comments

--- scripts/Pairings.js
+++ scripts/Pairings.js
@@ -12,7 +12,9 @@ const celebrities = getCelebrities()
 //corrected the function
 //the function below 的作用是：找到某一个 specific kid 的 matching star。
 const findCelebrityMatch = (kid, celebrityArray) => {
-    let kidCelebrity //??? can you just declare: let kidCelebrity 吗?
+    let kidCelebrity = null
+        // 因为已知kidCelebrity将会为一个string，所以也可以直接 declare： let kidCelebrity；
+        // 但是如果已知要declare的datatype将会是array，那就要declare： let kidCelebrity = []；
 
     for (const star of celebrityArray) {
         if (star.id === kid.celebrityId) {
